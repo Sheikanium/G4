e17c537ad67b1dda8d0e957a3a6be50641f210e0
Sheikanium <sheikanium@gmail.com>
2013-03-17 16:42:04 +0100

    Update resetLab.sh

--- lab/resetLab.sh
+++ lab/resetLab.sh
@@ -4,6 +4,7 @@
 
 rm G4.startup
 rm G4.disk
+rm G4.ready
 rm G0.disk
 rm G0.ready
 rm -r G4
@@ -11,5 +12,6 @@ rm lab.conf
 
 echo 'machines="G0"' >> lab.conf
 echo ' ' >> lab.conf
-echo 'G0[0]=ex' >> lab.conf
+echo 'G0[0]=tap,10.0.0.1,10.0.0.2' >> lab.conf
+echo 'G0[1]=ex' >> lab.conf
 echo 'G0[mem]=128' >> lab.conf
