8a0ae26873ed51101219779ee8997f547fb82471
Sheikanium <sheikanium@gmail.com>
2013-03-18 06:28:29 +0100

    Update test_config.sh

--- test_config.sh
+++ test_config.sh
@@ -9,7 +9,7 @@ INSTALLER_LOG=/var/log/my-installer.log
 apt-get update
 
 installnoninteractive(){
-  sudo bash -c "DEBIAN_FRONTEND=noninteractive aptitude install -q -y $* >> $INSTALLER_LOG"
+   bash -c "DEBIAN_FRONTEND=noninteractive aptitude install -q -y $* >> $INSTALLER_LOG"
 }
 
 installnoninteractive debian-archive-keyring
